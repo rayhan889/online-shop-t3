96a1c6a496aa76bd78d4654b880efa2dcc259cb4
rayhan889 <raydev1604@gmail.com>
2023-07-12 14:35:25 +0700

    Data can undefined or empty arr

--- src/components/list/products/data-table.tsx
+++ src/components/list/products/data-table.tsx
@@ -18,7 +18,7 @@ import {
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
-  data: TData[];
+  data: TData[] | [] | undefined;
 }
 
 export function DataTable<TData, TValue>({
@@ -26,7 +26,7 @@ export function DataTable<TData, TValue>({
   data,
 }: DataTableProps<TData, TValue>) {
   const table = useReactTable({
-    data,
+    data: data || [],
     columns,
     getCoreRowModel: getCoreRowModel(),
   });
