51097aa7f4737e45d877a25a527d55c5d195e40a
rayhan889 <raydev1604@gmail.com>
2023-07-12 14:34:33 +0700

    Remove max-w

--- src/components/dashboard/ContentArea.tsx
+++ src/components/dashboard/ContentArea.tsx
@@ -3,7 +3,7 @@ import type { PropsWithChildren } from "react";
 export default function ContentArea({ children }: PropsWithChildren) {
   return (
     <main className="min-h-screen bg-[#FAFAFA] py-6 md:pl-64">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">{children}</div>
+      <div className="mx-auto px-4 sm:px-6 md:px-8">{children}</div>
     </main>
   );
 }
